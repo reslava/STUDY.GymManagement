9097780c589715d1346dc06fba273f3a02e58a28
reslava <rafeslava@hotmail.com>
2025-12-18 19:01:47 +0100

    17 Implementing Controllers in the Presentation Layer

---
+++ src/GymManagement.Api/Controllers/SubscripionsController.cs
@@ -0,0 +1,16 @@
+﻿using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SubscriptionsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+    {
+        return Ok (request);
+    }
+
+}
